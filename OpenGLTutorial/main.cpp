352d86c4c8e26522fe88967587e3b702561129b2
Jesús Bartús Sancho <jesus.bartus@gmail.com>
2017-09-09 18:33:09 +0200

    Initial Commit

---
+++ OpenGLTutorial/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  OpenGLTutorial
+//
+//  Created by Jesús Bartús Sancho on 9/9/17.
+//  Copyright © 2017 Jesús Bartús Sancho. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
